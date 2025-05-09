4f16e68996b4e4ef8c8ec235e72fe85f5b27b5a4
IvahnMika <lobrio.mika.felipe@gmail.com>
2025-05-09 22:35:41 +0800

    commit!

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
 const rootElement = document.getElementById('root');
 
-
-
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement as HTMLElement);
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
       <App />
