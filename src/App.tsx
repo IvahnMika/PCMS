af70736e35ef21d452670ff09e94a44998d9ff2c
IvahnMika <lobrio.mika.felipe@gmail.com>
2025-05-09 22:20:14 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import About from './pages/About';
 const App = () => {
   return (
     
-    <Router>
+    <Router  basename="/PCMS">
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
         <Route path="/Home" element={<Home />} />
